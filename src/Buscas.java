ba575f5a2b904ad0f265805c38faaef537ec83ef
kauaswag <kaualongui2017@gmail.com>
2026-03-17 21:17:40 -0300

    v2

--- src/Buscas.java
+++ src/Buscas.java
@@ -5,11 +5,34 @@ public class Buscas {
         int pos = buscaSequencial(vetorDesordenado, 2.4);
         System.out.println("Localizado na posição (indice): " + pos);
 
+        // Busca binária
+        double[] vetorOrdenado = {1,2,4,8,10,11,12};
+        int pos2 = buscaBinaria(vetorOrdenado, 1);
+        System.out.println("Localizado na posição (indice): " + pos2);
     }
     public static int buscaSequencial(double[] vetor, double valor) {
         for (int i = 0; i < vetor.length; i++) {
             if (vetor[i] == valor) {
                 return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int buscaBinaria(double[] vetor, double valorPesquisado) {
+        int inicio = 0;
+        int fim = vetor.length - 1;
+
+        while (inicio <= fim) {
+            int meio = (inicio + fim) / 2;
+
+            if (vetor[meio] == valorPesquisado) {
+                return meio;
+            } 
+            if (valorPesquisado < vetor[meio]) {
+                fim = meio - 1;
+            } else {
+                inicio = meio + 1;
             }
         }
         return -1;
